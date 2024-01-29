4895960ca9793abbe6ab19c369d622e444e4d32e
Sebastian Gode <sebastian.gode@telekom.de>
2024-01-29 12:45:15 +0000

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- umn/source/conf.py
+++ umn/source/conf.py
@@ -104,9 +104,4 @@ html_static_path = ['_static']
 html_copy_source = False
 
 # -- Options for PDF output --------------------------------------------------
-latex_documents = [
-    ('index',
-     'None.tex',
-     u'Data Warehouse Service - User Guide',
-     u'OpenTelekomCloud', 'manual'),
-]
+latex_documents = []
